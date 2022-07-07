846da814cd680e223edef828c10ce258f6875c64
Nathan Wiens <nathan.wiens@gmail.com>
2022-07-07 11:10:15 -0700

    Updated app to use new disable_leds function

--- e300_run_dark/e300_run_dark.py
+++ e300_run_dark/e300_run_dark.py
@@ -1,9 +1,15 @@
-import cs
+from csclient import EventingCSClient
+cp = EventingCSClient('ports-status')
 from time import sleep
 
-APP_NAME = 'E300_RUN_DARK'
+APP_NAME = 'E-SERIES_RUN_DARK'
 
+cp.log('STARTED E300 RUN DARK APP')
+cp.put('config/system/disable_leds', True)
+
+"""
 while 1:
     for i in range(0, 18):
         cs.CSClient().put('/control/gpio/LED_BAR_{}'.format(i), 0)
     sleep(1)
+"""
